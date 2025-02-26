730a91dd9f9a00b081ce0c618921106e8683169a
syfu-demo <sksaifulla53@gmail.com>
2025-02-26 15:22:06 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 // require("dotenv").config({ path: "./env" });
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-connectDB();
+
 dotenv.config({
   path: "./env",
 });
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`SERVER is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!!", err);
+  });
 // // FIRST APPROACH BUT NOT GOOD FOR PRODUCTION GRADE
 // import express from "express";
 // const app = express();
