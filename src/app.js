0f98a84720455b245590afa75f29411a114107ec
syfu-demo <sksaifulla53@gmail.com>
2025-03-05 00:37:42 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,5 +14,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+app.use("/api/v1/users", userRouter);
+
+// hhtp://localhost:8000/api/v1/users/register
 export { app };
 // export default app;
